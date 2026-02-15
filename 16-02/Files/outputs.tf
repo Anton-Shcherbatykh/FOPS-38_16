ecf8de5d47bc2ddce01bd6bfdb1b41f41578321a
Anton <anton.shcherbatykh.2000@gmail.com>
2026-02-16 02:15:28 +0300

    Update outputs.tf

--- 16-02/Files/outputs.tf
+++ 16-02/Files/outputs.tf
@@ -6,12 +6,3 @@ output "VMs" {
     external_ip2 = yandex_compute_instance.platform_db.network_interface.0.nat_ip_address
   }
 }
-#/*
-#output "admin_server_info" {
-#  value = "${local.test_map.admin} is admin for ${local.test_list[2]} server based on OS ${local.servers.production["image"]} with ${local.servers.production["cpu"]} vcpu, ${local.servers.production["ram"]} ram and ${local.servers.production["disks"][0]}, ${local.servers.production["disks"][1]}, ${local.servers.production["disks"][2]}, ${local.servers.production["disks"][3]} virtual disks."
-#}
-#*/
-
-#output "admin_server_info" {
-#  value = "${local.test_map.admin} is admin for ${local.test_list[length(local.test_list)-1]} server based on OS ${local.servers[local.test_list[length(local.test_list)-1]]["image"]} with ${local.servers[local.test_list[length(local.test_list)-1]]["cpu"]} vcpu, ${local.servers[local.test_list[length(local.test_list)-1]]["ram"]} ram, and ${local.servers.production["disks"][0]}, ${local.servers.production["disks"][1]}, ${local.servers.production["disks"][2]}, ${local.servers.production["disks"][3]} virtual disks."
-#}
